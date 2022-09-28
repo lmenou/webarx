02e963c1c91d75c3c04f29ef562350bc8ee0ca95
lmenou <menl94629@gmail.com>
2022-09-28 17:55:02 +0200

    Correct cmake use

--- cmake/GetFTXUI.cmake
+++ cmake/GetFTXUI.cmake
@@ -3,5 +3,4 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/ArthurSonzogni/ftxui
   GIT_TAG c033ca61ae8fb264542d0326d1309e0a3bde945a)
 
-set(FTXUI_BUILD_EXAMPLES OFF)
 FetchContent_MakeAvailable(ftxui)
